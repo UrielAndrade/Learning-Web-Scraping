10c36e320aabc7776e9991ebd1fedf93b6f91fa7
UrielAndrade <andradeleiru@gmail.com>
2025-12-01 11:32:45 -0400

    Refatorar script Selenium para usar variáveis dinâmicas e melhorar a legibilidade

--- UsandoSelenium.py
+++ UsandoSelenium.py
@@ -5,22 +5,42 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+matriculaSiap = "2024102020056"
+url = f"https://suap.ifro.edu.br/edu/aluno/{matriculaSiap}/"
+
 options = Options()
-options.add_argument("--user-data-dir=/home/uriel/.config/google-chrome")
+options.add_argument("--user-data-dir=/home/uriel/.config/selenium-profile")
 options.add_argument("--profile-directory=Default")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-# options.add_argument("--remote-debugging-port=9222")
+service = Service() 
 
-driver = webdriver.Chrome(options=options)
-driver.get("https://suap.ifro.edu.br/edu/aluno/2024102020056/")
+driver = webdriver.Chrome(service=service, options=options)
+driver.get(url)
 
 wait = WebDriverWait(driver, 15)
 
-ITEM = ""
+dados = [
+  "//div[contains(@class,'list-item')][./dt[text()='Nome']]/dd",
+  "//div[contains(@class,'list-item')][./dt[text()='Matrícula']]/dd",
+  "//div[contains(@class,'list-item')][./dt[text()='E-mail Acadêmico']]/dd"
+  ]
 
-elemento = wait.until(
-  EC.presence_of_element_located((By.CSS_SELECTOR, ITEM ))
-)
-print("Resultado:", elemento.text)
-input("Pressione ENTER para fechar...") 
+for dado in dados:
+  elemento = wait.until(
+    EC.presence_of_element_located((By.XPATH, dado)) # By.CSS_SELECTOR - para CSS
+    )
+  print("Resultado:", elemento.text)
+
+
+input("Pressione ENTER para fechar...")
+
+
+
+
+# pega tudo  
+# ITEM = "dl.definition-list.flex" #nao deixe vazio 
+
+  # nome = "//div[contains(@class,'list-item')][./dt[text()='Nome']]/dd"
+  # matricula = "//div[contains(@class,'list-item')][./dt[text()='Matrícula']]/dd"
+  # e-mailAcademico = "//div[contains(@class,'list-item')][./dt[text()='E-mail Acadêmico']]/dd"
